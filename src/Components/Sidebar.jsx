1614fb85607d065c98b2e7a4fa376d8a12f58ad0
Kshitiz Behera <kshitizbehera@Kshitizs-MacBook-Pro-2.local>
2026-04-19 18:39:49 +0530

    feat: added basic responsive sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,20 +2,44 @@ import { Link } from "react-router-dom";
 
 export default function Sidebar() {
   return (
-    <div style={{
-      width: "220px",
-      background: "#000",
-      color: "#fff",
-      padding: "20px",
-      height: "100vh"
-    }}>
+    <div
+      style={{
+        width: window.innerWidth < 600 ? "80px" : "220px",
+        background: "#000",
+        color: "#fff",
+        padding: "20px",
+        height: "100vh"
+      }}
+    >
       <h2 style={{ marginBottom: "20px" }}>Spotify</h2>
 
-      <Link to="/" style={{ display: "block", margin: "10px 0", color: "white" }}>
+      {/* Home Link */}
+      <Link
+        to="/"
+        style={{
+          display: "block",
+          margin: "10px 0",
+          color: "white",
+          textDecoration: "none"
+        }}
+        onMouseOver={(e) => (e.target.style.color = "#1db954")}
+        onMouseOut={(e) => (e.target.style.color = "white")}
+      >
         Home
       </Link>
 
-      <Link to="/search" style={{ display: "block", margin: "10px 0", color: "white" }}>
+      {/* Search Link */}
+      <Link
+        to="/search"
+        style={{
+          display: "block",
+          margin: "10px 0",
+          color: "white",
+          textDecoration: "none"
+        }}
+        onMouseOver={(e) => (e.target.style.color = "#1db954")}
+        onMouseOut={(e) => (e.target.style.color = "white")}
+      >
         Search
       </Link>
     </div>
