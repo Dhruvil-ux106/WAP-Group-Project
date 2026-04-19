1ac4e2adf3a38ef506d5110217a37cefeb18a1bc
Dhruvil-ux106 <getdhruvil15@gmail.com>
2026-04-19 19:22:37 +0530

    rendered playlist

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,9 +1,32 @@
 import { useParams } from "react-router-dom";
 import { playlists } from "../data/data";
+import SongRow from "./components/SongRow";
+
+
 
 export default function Playlist() {
   const { id } = useParams();
   const playlist = playlists.find(p => p.id == id);
+  return (
+  <div style={{ padding: "20px", color: "white" }}>
+    
+    {/* Playlist Header */}
+    <div style={{ marginBottom: "20px" }}>
+      <h1 style={{ fontSize: "28px" }}>{playlist.title}</h1>
+      <p style={{ color: "#aaa" }}>
+        {playlist.songs.length} songs
+      </p>
+    </div>
 
-  return <h1 style={{ color: "white" }}>{playlist.title}</h1>;
+    {/* Songs List */}
+    <div>
+      {playlist.songs.map((s, i) => (
+        <SongRow key={i} name={s} />
+      ))}
+    </div>
+
+  </div>
+);
+
+  
 }
