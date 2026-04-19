b2aa1640b9b4e8b3f754d324bc10defc8a07d923
Dhruvil-ux106 <getdhruvil15@gmail.com>
2026-04-19 19:19:19 +0530

    modified playlist

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,3 +1,9 @@
+import { useParams } from "react-router-dom";
+import { playlists } from "../data/data";
+
 export default function Playlist() {
-  return <h1 style={{ color: "white" }}>Playlist</h1>;
+  const { id } = useParams();
+  const playlist = playlists.find(p => p.id == id);
+
+  return <h1 style={{ color: "white" }}>{playlist.title}</h1>;
 }
