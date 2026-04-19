810141120b1aa9b4c9769bb4d890782452f4eeca
Dhruvil-ux106 <getdhruvil15@gmail.com>
2026-04-19 19:18:07 +0530

    added Navigation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import { playlists } from "../data/data";
 import Card from "./components/Card";
+import { useNavigate } from "react-router-dom";
+
+const navigate = useNavigate();
 
 export default function Home() {
   return (
     <div style={{ padding: "20px" }}>
       {playlists.map((p) => (
-        <Card key={p.id} title={p.title} />
+        <Card
+    key={p.id}
+    title={p.title}
+    onClick={() => navigate(`/playlist/${p.id}`)}
+/>
       ))}
     </div>
   );
