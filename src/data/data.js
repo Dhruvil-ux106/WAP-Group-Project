f82c37d36dae16f4f35c32f3c172eb711ad3c8db
Dhruvil-ux106 <getdhruvil15@gmail.com>
2026-04-19 19:16:05 +0530

    added data.js

---
+++ src/data/data.js
@@ -0,0 +1,12 @@
+export const playlists = [
+  {
+    id: 1,
+    title: "Chill Hits",
+    songs: ["Song 1", "Song 2"]
+  },
+  {
+    id: 2,
+    title: "Top Songs",
+    songs: ["Song A", "Song B"]
+  }
+];
