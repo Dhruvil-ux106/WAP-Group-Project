4320d8c286114692928cedca753cae43157fbf96
Dhruvil-ux106 <getdhruvil15@gmail.com>
2026-04-19 19:23:19 +0530

    fixed SongRow

--- src/Components/SongRow.jsx
+++ src/Components/SongRow.jsx
@@ -1,6 +1,14 @@
 export default function SongRow({ name }) {
   return (
-    <div style={{ padding: "10px", color: "white" }}>
+    <div
+      style={{
+        padding: "10px",
+        borderBottom: "1px solid #333",
+        cursor: "pointer"
+      }}
+      onMouseOver={(e) => (e.currentTarget.style.background = "#282828")}
+      onMouseOut={(e) => (e.currentTarget.style.background = "transparent")}
+    >
       {name}
     </div>
   );
