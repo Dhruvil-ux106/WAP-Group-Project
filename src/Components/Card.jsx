8b468db8524965a61a1113a7d43df271c825dec2
Dhruvil-ux106 <getdhruvil15@gmail.com>
2026-04-19 19:24:36 +0530

    styled card better

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,12 +3,11 @@ export default function Card({ title, onClick }) {
     <div
       onClick={onClick}
       style={{
-        background: "#181818",
-        padding: "15px",
-        margin: "10px",
-        color: "white",
-        cursor: "pointer"
-      }}
+  background: "#181818",
+  padding: "20px",
+  margin: "10px",
+  borderRadius: "8px"
+}}
     >
       {title}
     </div>
