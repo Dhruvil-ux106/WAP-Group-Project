85ca1392313892d245cee64fd95da6001fb988f9
Dhruvil-ux106 <getdhruvil15@gmail.com>
2026-04-19 19:26:04 +0530

    displayed components on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,28 @@ import { playlists } from "../data/data";
 import Card from "./components/Card";
 import { useNavigate } from "react-router-dom";
 
-const navigate = useNavigate();
-
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <div style={{ padding: "20px" }}>
-      {playlists.map((p) => (
-        <Card
-    key={p.id}
-    title={p.title}
-    onClick={() => navigate(`/playlist/${p.id}`)}
-/>
-      ))}
+
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(150px, 1fr))",
+          gap: "15px"
+        }}
+      >
+        {playlists.map((p) => (
+          <Card
+            key={p.id}
+            title={p.title}
+            onClick={() => navigate(`/playlist/${p.id}`)}
+          />
+        ))}
+      </div>
+
     </div>
   );
 }
