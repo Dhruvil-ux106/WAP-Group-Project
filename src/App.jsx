6b791c4ec8097472c411502e5f0677d40a5dcaef
Kshitiz Behera <kshitizbehera@Kshitizs-MacBook-Pro-2.local>
2026-04-19 18:13:34 +0530

    Navbar imported to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Sidebar from "./Components/Sidebar";
+import Navbar from "./Components/Navbar"; // ADD THIS
 
 export default function App() {
   return (
     <div style={{ display: "flex" }}>
       <Sidebar />
-      <div>Main</div>
+
+      <div style={{ flex: 1 }}>
+        <Navbar />   {/* ADD HERE */}
+        <div>Main</div>
+      </div>
+
     </div>
   );
 }
